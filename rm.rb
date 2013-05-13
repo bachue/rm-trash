0efbc8c793884985ba670b965eab3464e83fe592
Bachue Zhou <bachue.shu@gmail.com>
2013-05-13 16:25:03 +0800

    Refactor code to use 1 apple script statement to delete multiple files

--- rm.rb
+++ rm.rb
@@ -1,53 +1,73 @@
 #!/usr/bin/env ruby
 require 'optparse'
 require 'pp'
+require 'open3'
 
-retval = 0
+$retval = 0
 
 def parse_options!
   options = {}
   OptionParser.new do |opts|
-    opts.on('-v', nil, 'Be verbose when deleting files, showing them as they are removed.') do
+    opts.on('-v', 'Be verbose when deleting files, showing them as they are removed.') do
       options[:verbose] = true
     end
   end.parse!
   options
 end
 
-def do_rm! to_delete = [], options = {}
-  to_delete.each do |file|
-    do_error_handling do
-      abs_file = File.expand_path(file)
-      if File.exists?(abs_file)
-        rm(abs_file)
-        puts file if options[:verbose]
-      else
-        $stderr.puts "rm: #{file}: No such file or directory"
-        retval = 1
-      end
+def do_rm! files = [], options = {}
+  abs_files = []
+  relative_files = []
+  files.each do |file|
+    abs_file = File.expand_path(file)
+    if File.exists?(abs_file)
+      abs_files << abs_file
+      relative_files << file
+    else
+      $stderr.puts "rm: #{file}: No such file or directory"
+      $retval = 1
     end
   end
+
+  rm(abs_files)
+  relative_files.each {|file| puts file} if options[:verbose]
 end
 
-# To call AppleScript to delete a file
+# To call AppleScript to delete a list of file
 # file param must be absolute path
-def rm file
+def rm files
+  return if files.empty?
   do_error_handling do
-    `osascript -e 'tell app "Finder" to delete POSIX file "#{file}"'`
+    _, _, err = Open3.popen3 <<-CMD
+      osascript -e '
+        tell app "Finder"
+          #{files.map {|file| "delete POSIX file \"#{file}\"" }.join("\n")}
+        end tell
+      '
+    CMD
+    if error = err.gets
+      $retval = 1
+      $stderr.puts error_message(error)
+    end
   end
 end
 
 def do_error_handling *args
   begin
-    output = yield(*args)
+    yield(*args)
   rescue
-    $stderr.puts "Encounter an error when call AppleScript to delete file `#{file}'"
-    $stderr.puts "Output: #{output}"
-    $stderr.puts "Error Message: #{$!}\n#{$@.join("\n")}"
-    $stderr.puts "Global Variables: #{ pp global_variables.inject({}) {|h, gb| h[gb] = eval(gb); h} }"
-    $stderr.puts "Instance Variables: #{ pp instance_variables.inject({}) {|h, ib| h[ib] = instance_variable_get(ib); h} }"
-    $stderr.puts "It should be a bug, please report this problem to bachue.shu@gmail.com!"
+    $stderr.puts error_message
   end
+end
+
+def error_message output = nil
+  """
+Error: #{"Output: #{output.strip}" if output}
+Error Message: #{$! ? "#{$!}\n#{$@.join("\n")}" : 'no exception thrown'}
+Global Variables: #{ PP.pp(global_variables.inject({}) {|h, gb| h[gb] = eval(gb); h}, '').strip }
+Instance Variables: #{ PP.pp(instance_variables.inject({}) {|h, ib| h[ib] = instance_variable_get(ib); h}, '').strip }
+It should be a bug, please report this problem to bachue.shu@gmail.com!
+  """
 end
 
 do_error_handling do
@@ -55,4 +75,4 @@ do_error_handling do
   do_rm! ARGV, options
 end
 
-exit retval
+exit $retval
