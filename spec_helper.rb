b9bc12a4ca4ab1c8502431f19b6070418415d027
Bachue Zhou <bachue.shu@gmail.com>
2013-05-14 12:02:53 +0800

    Refactor test cases

--- spec_helper.rb
+++ spec_helper.rb
@@ -23,3 +23,55 @@ class Array
     sort {rand * 2 - 1}
   end
 end
+
+def create_files root = Dir.mktmpdir
+  @files = (1..5).map {|i|
+    file = "#{root}/file_#{i}"
+    FileUtils.touch file
+  }.flatten
+end
+
+def create_empty_dirs root = Dir.mktmpdir
+  @empty_dirs = ('a'..'e').map {|i|
+    dir = "#{root}/empty_dir_#{i}"
+    FileUtils.mkdir dir
+  }.flatten
+end
+
+def create_non_empty_dirs root = Dir.mktmpdir
+  @all_files_in_non_empty_dirs = []
+  @non_empty_dirs = ('a'..'e').map {|i|
+    dir = "#{root}/non_empty_dir_#{i}"
+    FileUtils.mkdir(dir).tap {|dirs|
+      @all_files_in_non_empty_dirs.concat FileUtils.touch "#{dirs.first}/file"
+    }
+  }.flatten
+end
+
+def create_hierarchical_dirs root = Dir.mktmpdir
+  @hierachical_files, @all_files_in_hierachical_files = [], []
+  ('a'..'e').each {|i|
+    dir = "#{root}/hierachical_dir_#{i}"
+    FileUtils.mkdir(dir).tap {|dirs|
+      ('a'..'e').each {|i|
+        @all_files_in_hierachical_files.concat FileUtils.mkdir("#{dirs.first}/dir_#{i}").tap { |dirs|
+          ('a'..'e').each {|i|
+            @all_files_in_hierachical_files.concat FileUtils.mkdir("#{dirs.first}/dir_#{i}").tap { |dirs|
+              ('a'..'e').each {|i|
+                @all_files_in_hierachical_files.concat FileUtils.touch("#{dirs.first}/file_#{i}")
+              }
+            }
+            @all_files_in_hierachical_files.concat FileUtils.touch("#{dirs.first}/file_#{i}")
+          }
+        }
+        @all_files_in_hierachical_files.concat FileUtils.touch("#{dirs.first}/file_#{i}")
+      }
+      @hierachical_files.concat dirs
+      @all_files_in_hierachical_files.concat dirs
+    }
+    FileUtils.touch("#{dirs.first}/file_#{i}").tap {|files|
+      @hierachical_files.concat files
+      @all_files_in_hierachical_files.concat files
+    }
+  }
+end
--- test.rb
+++ test.rb
@@ -3,11 +3,8 @@ require 'spec_helper'
 
 describe 'test `rm`' do
   before(:each) do
-    root = Dir.mktmpdir
-    @files = (1..5).map {|i|
-      file = "#{root}/file_#{i}"
-      FileUtils.touch file
-    }.flatten
+    create_files
+    create_empty_dirs
   end
 
   it 'should delete all files' do
@@ -25,15 +22,21 @@ describe 'test `rm`' do
     @not_existed_files.each {|f| stderr.should =~ /rm: #{f}: No such file or directory\n/ }
     @files.each {|f| f.should_not be_exists}
   end
+
+  it 'should add -d if try to rm a directory' do
+    @dirs = @empty_dirs.disorder
+    @all_files = (@files + @dirs).disorder
+    stdout, stderr = rm(*@all_files)
+    stdout.should be_nil
+    @dirs.each {|f| stderr.should =~ /rm: #{f}: is a directory\n/ }
+    @files.each {|f| f.should_not be_exists}
+    @dirs.each {|f| f.should be_exists}
+  end
 end
 
 describe 'test `rm -v`' do
   before(:each) do
-    root = Dir.mktmpdir
-    @files = (1..5).map {|i|
-      file = "#{root}/file_#{i}"
-      FileUtils.touch file
-    }.flatten
+    create_files
   end
 
   it 'should delete all files' do
@@ -55,39 +58,25 @@ end
 
 describe 'test `rm -d`' do
   before(:each) do
-    root = Dir.mktmpdir
-    @files = (1..5).map {|i|
-      file = "#{root}/file_#{i}"
-      FileUtils.touch file
-    }.flatten
-    @empty_dirs = ('a'..'e').map {|i|
-      dir = "#{root}/empty_dir_#{i}"
-      FileUtils.mkdir dir
-    }.flatten
-    @not_empty_dirs = ('f'..'j').map {|i|
-      dir = "#{root}/empty_dir_#{i}"
-      FileUtils.mkdir(dir).tap {|tap|
-        FileUtils.touch "#{dir}/file"
-      }
-    }.flatten
+    create_files
+    create_empty_dirs
+    create_non_empty_dirs
   end
 
-  it 'should add -d if try to rm a directory' do
-    @dirs = (@empty_dirs + @not_empty_dirs).disorder
-    @all_files = (@files + @dirs).disorder
-    stdout, stderr = rm(*@all_files)
+  it 'can rm empty directories' do
+    @all_files = (@files + @empty_dirs).disorder
+    stdout, stderr = rm('-d', *@all_files)
     stdout.should be_nil
-    @dirs.each {|f| stderr.should =~ /rm: #{f}: is a directory\n/ }
-    @files.each {|f| f.should_not be_exists}
-    @dirs.each {|f| f.should be_exists}
+    stderr.should be_nil
+    @all_files.each {|f| f.should_not be_exists}
   end
 
   it 'can\'t rm a directory which is not empty ever add -d' do
     @enable_to_delete = (@files + @empty_dirs).disorder
-    @all_files = (@enable_to_delete + @not_empty_dirs).disorder
+    @all_files = (@enable_to_delete + @non_empty_dirs).disorder
     stdout, stderr = rm('-d', *@all_files)
-    @not_empty_dirs.each {|f| stderr.should =~ /rm: #{f}: Directory not empty\n/ }
+    @non_empty_dirs.each {|f| stderr.should =~ /rm: #{f}: Directory not empty\n/ }
     @enable_to_delete.each {|f| f.should_not be_exists}
-    @not_empty_dirs.each {|f| f.should be_exists}
+    @non_empty_dirs.each {|f| f.should be_exists}
   end
 end
