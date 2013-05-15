8b6a0167c5d8d2697134a586ea45a3c5a0f84d86
Bachue Zhou <bachue.shu@gmail.com>
2013-05-15 09:31:18 +0800

    Now we have add color feature to String

---
+++ lib/string_color.rb
@@ -0,0 +1,28 @@
+class String
+  COLORS =
+  {
+    "black"   => 0,
+    "red"     => 1,
+    "green"   => 2,
+    "yellow"  => 3,
+    "blue"    => 4,
+    "purple"  => 5,
+    "magenta" => 5,
+    "cyan"    => 6,
+    "white"   => 7
+  }
+
+  COLORS.each_pair do |color, value|
+    define_method(color) do
+      "\033[0;#{30+value}m#{self}\033[0m"
+    end
+
+    define_method("bright_#{color}") do
+      "\033[1;#{30+value}m#{self}\033[0m"
+    end
+  end
+
+  def bold
+    "\e[1m#{self}\e[0m"
+  end
+end
